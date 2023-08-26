9c0110c62e155057bca558e0122e92bbe286b02f
Ethan-Lem05 <88153797+Ethan-Lem05@users.noreply.github.com>
2023-08-26 00:17:16 -0400

    Rendered the rest of the files

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ from flask import render_template;
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("home.html");
+
+@app.route("/survey")
+def questions():
+    return render_template("questions.html");
+
+@app.route("/trending")
+def trending():
+    return render_template("trending.html");
 
 if __name__ == "__main__":
     app.run(debug = True);
