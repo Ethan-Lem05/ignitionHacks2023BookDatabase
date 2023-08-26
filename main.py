77027d1069c4034b9aec920cba69142b651ec053
Ethan-Lem05 <88153797+Ethan-Lem05@users.noreply.github.com>
2023-08-26 12:49:34 -0400

    Form and POST request handling

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from flask import Flask
 from flask import render_template;
+from flask import request;
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("home.html");
+    return render_template("index.html");
 
-@app.route("/survey")
+@app.route("/survey", methods = ['GET', 'POST'])
 def questions():
-    return render_template("questions.html");
+    if request.method == 'GET': #handles GET requests for the survey
+        return render_template("questions.html");
+    elif request.method == 'POST': #handles POST requests from the survey 
+        #takes information about the users name 
+        name = request.form["name"];
+        #gathers information about race 
+        race = request.form.getlist("race");
+        gender = request.form.getlist("gender");
+        sexuality = request.form.getlist("sexuality");
+        religion = request.form.getlist("religion");
+        print(race + gender + sexuality + religion);
+        return "hello";
+
+        
 
 @app.route("/trending")
 def trending():
@@ -17,3 +31,9 @@ def trending():
 
 if __name__ == "__main__":
     app.run(debug = True);
+
+#returns the adequete response page with the included books 
+def parse_questions_to_query(request):
+    #process race 
+    race = request.form
+
