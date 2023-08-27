180ea7ad27ed0377ecce3dbcb6b3493d966a5ebd
YashC <59297748+Yoshi211@users.noreply.github.com>
2023-08-26 23:42:49 -0400

    New Navbar and footer to React
    also changed the url_for to /

--- templates/react_files/footer.js
+++ templates/react_files/footer.js
@@ -5,43 +5,40 @@ function Footer() {
     <div>
       {/* <!-- FOOTER --> */}
       <div className="container">
-        <footer className="py-3 my-4">
-          <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-            <li className="nav-item">
-              <a href="home.html" className="nav-link px-2 text-body-secondary">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                href="trending.html"
-                className="nav-link px-2 text-body-secondary"
-              >
-                Trending
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                href="response.html"
-                className="nav-link px-2 text-body-secondary"
-              >
-                Book Lists
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                href="questions.html"
-                className="nav-link px-2 text-body-secondary"
-              >
-                Q&A
-              </a>
-            </li>
-          </ul>
-          <p className="text-center text-body-secondary">
-            © Book Website's name
-          </p>
-        </footer>
-      </div>
+      <footer className="py-3 my-4">
+        <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+          <li className="nav-item">
+            <a
+              href="/"
+              className="nav-link px-2 text-body-secondary"
+              >Home</a
+            >
+          </li>
+          <li className="nav-item">
+            <a
+              href="/trending"
+              className="nav-link px-2 text-body-secondary"
+              >Trending</a
+            >
+          </li>
+          <li className="nav-item">
+            <a
+              href="/response"
+              className="nav-link px-2 text-body-secondary"
+              >Book Lists</a
+            >
+          </li>
+          <li className="nav-item">
+            <a
+              href="/questions"
+              className="nav-link px-2 text-body-secondary"
+              >Q&A</a
+            >
+          </li>
+        </ul>
+        <p className="text-center text-body-secondary">© Book Website's name</p>
+      </footer>
+     </div>
     </div>
   );
 }
--- templates/react_files/navbar.js
+++ templates/react_files/navbar.js
@@ -2,88 +2,57 @@ import React from "react"
 
 function Navbar() {
     return (
-      <div>
-        {/* <!-- Navigation Bar --> */}
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="home.html">
-              Book Website
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">Book Website</a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a
+                className="nav-link active"
+                aria-current="page"
+                href="/"
+                >Home</a
+              >
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/trending">Trending</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/response">Book Lists</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/questions"> Q&A </a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Enter a genre/ name of an author"
+              aria-label="Enter a genre/ name of an author "
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
             </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/home">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="trending.html">
-                    Trending
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="response.html">
-                    Book Lists
-                  </a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="questions.html"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Q&A
-                  </a>
-                  <ul className="dropdown-menu">
-                    <li>
-                      <a className="dropdown-item" href="questions.html">
-                        Common Question 1
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="questions.html">
-                        Common Question 2
-                      </a>
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="questions.html">
-                        Other Questions
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-              <form className="d-flex" role="search">
-                <input
-                  className="form-control me-2"
-                  type="search"
-                  placeholder="Please enter a book title, genre or name of an author"
-                  aria-label="Please enter a book title, genre or name of an author "
-                />
-                <button className="btn btn-outline-success" type="submit">
-                  Search
-                </button>
-              </form>
-            </div>
-          </div>
-        </nav>
+          </form>
+        </div>
       </div>
+    </nav>
+    </div>
     );
   }
 
