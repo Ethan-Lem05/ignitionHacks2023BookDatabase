07ab6fc631213904623ba30ff5eb0a68d8c9b556
YashC <59297748+Yoshi211@users.noreply.github.com>
2023-08-26 23:50:42 -0400

    New Questions Code
    Converted to React (now working)

--- templates/react_files/questions.js
+++ templates/react_files/questions.js
@@ -1,104 +1,306 @@
 import React from "react"
 import Navbar from "./navbar"
-// import Footer from "./footer"
+import Footer from "./footer"
 
 function Questions (){
     return (
      <div>
         {/* <!-- Q&A PAGE --> */}
-    
         {/* <!-- Navigation Bar --> */}
         <Navbar/>
-    
-    <div class="container">
-      <footer class="py-3 my-4">
-        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-          <li class="nav-item">
-            <a href="{{ url_for('home')}}" class="nav-link px-2 text-body-secondary"
-               
-<form action="#" method = "post">
 
-      {/* <!-- Asks for name of the person-->> */}
-      <p style = "margin-left: 50px;"> name </p>
-      <p style = "margin-left: 50px;"> <input type="text" name = "name"> </input> </p>
+        {/* <!-- Q&A Form --> */}
+       <form action="#" method="post" className="row g-3 needs-validation" novalidate>
+       <div className="mb-3 needs-validation">
+        <label for="validationCustom01" className="form-label"
+          >Please enter your first and last name</label
+        >
 
+        <div className="input-group has-validation">
+          <span className="input-group-text">First and last name</span>
+          <input
+            type="text"
+            className="form-control"
+            id="validationCustomUsername"
+            aria-describedby="inputGroupPrepend"
+            required
+          />
+          <div className="invalid-feedback">First and Last Name</div>
+        </div>
+      </div>
+      {/* <!-- Race  --> */}
+      <div className="mb-3 needs-validation">
+        <label for="validationCustom01" className="form-label"
+          >Please select the race</label
+        >
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" for="flexCheckDefault"> Black </label>
+        </div>
 
-      <p style = "margin-left: 50px;"> Race </p>
-        <ul>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "race" value = "Black"> Black </input> </p>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "race" value =  "East_Asian"> East Asian </input> </p>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "race" value = "Latinx"> Latinx </input> </p>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "race" value = "South_Asian"> South Asian </input> </p>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "race" value = "South_Asian"> South Asian </input> </p>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "race" value = "White"> White </input> </p>
-        </ul>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" for="flexCheckDefault">
+            East Asian
+          </label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" for="flexCheckDefault">
+            Latinx
+          </label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" for="flexCheckDefault">
+            South Asian
+          </label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" for="flexCheckDefault"> White </label>
+        </div>
+      </div>
+      {/* <!-- Gender --> */}
+      <div className="mb-3 needs-validation">
+        <label for="validationCustom01" className="form-label"
+          >Please select the gender</label
+        >
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" for="flexCheckDefault"> Male </label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" for="flexCheckDefault">
+            Female
+          </label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" for="flexCheckDefault">
+            Non-binary
+          </label>
+        </div>
+      </div>
+      {/* <!-- Sexuality --> */}
+      <div className="mb-3 needs-validation">
+        <label for="validationCustom01" className="form-label"
+          >Please select the Sexuality</label
+        >
 
-        <p style = "margin-left: 50px;"> Gender </p>
-        <ul>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "gender" value = "Male"> Male </input> </p>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "gender" value = "Female"> Female </input> </p>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "gender" value = "Non-binary"> Non-binary </input> </p>
-        </ul>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" for="flexCheckDefault">
+            Gay/Lesbian
+          </label>
+        </div>
 
-        <p style = "margin-left: 50px;"> Sexuality </p>
-        <ul>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "sexuality" value = "Gay/Lesbian"> Gay/Lesbian </input> </p>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "sexuality" value = "Bisexual"> Bisexual </input> </p>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "sexuality" value = "Queer"> Queer </input> </p>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "sexuality" value = "Intersex"> Intersex </input> </p>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "sexuality" value = "Asexual"> Asexual </input> </p>
-        </ul>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" for="flexCheckDefault">
+            Bisexual
+          </label>
+        </div>
 
-        <p style = "margin-left: 50px;"> Religion </p>
-        <ul>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "religion" value = "Athiest"> Athiest </input> </p>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "religion" value = "Hindu"> Hinduism </input> </p>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "religion" value = "Islam"> Islam </input> </p>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "Religion" value = "Christianity"> Christianity </input> </p>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "religion" value = "Agnostic"> Agnostic </input> </p>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "religion" value = "Buddhism"> Buddhism </input> </p>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "religion" value = "Judaism"> Judaism </input> </p>
-          <p style = "margin-left: 50px;"> <input type="checkbox" name = "religion" value = "Sikhism"> Sikhism </input> </p>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" for="flexCheckDefault"> Queer </label>
+        </div>
 
-        </ul>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" for="flexCheckDefault">
+            Intersex
+          </label>
+        </div>
 
-      <p style = "margin-left: 50px;"> <input type="submit" value = "value"/> </p>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" for="flexCheckDefault">
+            Asexual
+          </label>
+        </div>
+      </div>
+      {/* <!-- Religion --> */}
+      <div className="mb-3 needs-validation">
+        <label for="validationCustom01" className="form-label"
+          >Please select the Religion</label
+        >
+
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" for="flexCheckDefault">
+            Athiest
+          </label>
+        </div>
+
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" for="flexCheckDefault">
+            Hinduism
+          </label>
+        </div>
+
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" for="flexCheckDefault"> Islam </label>
+        </div>
+
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" for="flexCheckDefault">
+            Christianity
+          </label>
+        </div>
+
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" for="flexCheckDefault">
+            Agnostic
+          </label>
+        </div>
+
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" for="flexCheckDefault">
+            Buddhism
+          </label>
+        </div>
+
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" for="flexCheckDefault">
+            Judaism
+          </label>
+        </div>
+
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" for="flexCheckDefault">
+            Sikhism
+          </label>
+        </div>
+
+        <div className="col-12">
+          <button className="btn btn-primary" type="submit">Submit form</button>
+        </div>
+      </div>
     </form>
 
-    {/* <!-- FOOTER --> */}
-    <div class="container">
-      <footer class="py-3 my-4">
-        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-          <li class="nav-item">
-            <a href="home.html" class="nav-link px-2 text-body-secondary"
-              >Home</a>
-          </li>
-            <a href="{{ url_for('trending')}}" class="nav-link px-2 text-body-secondary"
-
-              >Trending</a
-            >
-          </li>
-          <li class="nav-item">
-
-            <a href="{{ url_for('response')}}" class="nav-link px-2 text-body-secondary"
-              >Book Lists</a
-            >
-          </li>
-          <li class="nav-item">
-            <a href="{{ url_for('questions')}}" class="nav-link px-2 text-body-secondary"
-              >Q&A</a
-              >
-          </li>
-        </ul>
-        <p class="text-center text-body-secondary">© Book Website's name</p>
-        </footer>
-    </div>
-     
-    {/* <script
+    {/* <!-- Footer --> */}
+    <Footer/>
+    
+    <script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
       crossorigin="anonymous"
-    ></script> */}
+    ></script>
     </div>
    );
 }
